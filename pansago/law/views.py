5bd0bae6eeaf535797b62165e4d2a572f00d9eab
goatson <goatson@email.mail>
2020-01-09 16:43:54 +0900

    1

--- pansago/law/views.py
+++ pansago/law/views.py
@@ -4,14 +4,10 @@ from .models import Prec
 import csv
 
 def index(request) :
-    with open('C:/Users/admin/Documents/pansago/law_list_detail.csv', 'r', encoding='utf-8') as f:
+    with open('/static/data/law_list_detail.csv', 'r', encoding='utf-8') as f:
         rdr = csv.reader(f)
-        
         for line in rdr:
             law_no = line[0]
-
-            print(law_no)
-
             law_title = line[1]
             law_event_no  = line[2]
             law_date	 = line[3]
@@ -26,6 +22,8 @@ def index(request) :
             law_ref   = line[12]
             law_ref_precedent  = line[13]
             law_content   = line[14]
+
+            print(law_no)
                 
             prec = Prec(law_no=law_no, law_title=law_title, law_event_no=law_event_no, law_date=law_date, law_seongo=law_seongo, law_court_name=law_court_name, law_court_code=law_court_code, law_event_type=law_event_type, law_event_code=law_event_code, law_result=law_result, law_judge= law_judge, law_judge_summary= law_judge_summary, law_ref=law_ref, law_ref_precedent=law_ref_precedent, law_content=law_content)
             prec.save()
