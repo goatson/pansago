d62e16dcfa2215cb4c2531268c831d60576d65e4
ducklee89 <ducklee89@email.mail>
2020-01-11 21:27:21 +0900

    dy

--- pansago/law/views.py
+++ pansago/law/views.py
@@ -13,23 +13,23 @@ from prec_word2vec import precUsingModel
 def index(request) :
     if request.method == "GET":
         # with open('C:/Users/admin/Documents/pansago/law_list_detail.csv', 'r', encoding='utf-8') as f:
-        #     csv.field_size_limit(100000000)
-        #     rdr = csv.reader(f)
-        #     for line in rdr:
-        #         law_no = line[0]
-        #         law_title = line[1]
-        #         law_event_no  = line[2]
-        #         law_date	 = line[3]
-        #         law_seongo  = line[4]
-        #         law_court_name = line[5]
-        #         law_event_type  = line[7]
-        #         law_result   = line[9]
-        #         law_content   = line[14]
+            # csv.field_size_limit(100000000)
+            # rdr = csv.reader(f)
+            # for line in rdr:
+            #     law_no = line[0]
+            #     law_title = line[1]
+            #     law_event_no  = line[2]
+            #     law_date	 = line[3]
+            #     law_seongo  = line[4]
+            #     law_court_name = line[5]
+            #     law_event_type  = line[7]
+            #     law_result   = line[9]
+            #     law_content   = line[14]
 
-        #         print(law_no)
+            #     print(law_no)
                     
-        #         prec = Prec(law_no=law_no, law_title=law_title, law_event_no=law_event_no, law_date=law_date, law_seongo=law_seongo, law_court_name=law_court_name, law_event_type=law_event_type, law_result=law_result, law_content=law_content)
-        #         prec.save()
+            #     prec = Prec(law_no=law_no, law_title=law_title, law_event_no=law_event_no, law_date=law_date, law_seongo=law_seongo, law_court_name=law_court_name, law_event_type=law_event_type, law_result=law_result, law_content=law_content)
+            #     prec.save()
 
         return render(request, 'index.html')  ## templates 밑에 바로 읽음
     
@@ -69,3 +69,8 @@ def preclist(request) :
         prec_num = list(range(len(precList)+1))
         # print(prec_num)
         return render(request, 'precList.html', {'prec_num' : prec_num, 'precList' : precList, 'posts':posts, 'pageList':pageList, 'searchtype':searchtype, 'searchkeyword':searchkeyword})
+
+
+
+
+        dictionarylist_
